75643cb027de713c6e9b5929d78bbad82d64a13f
syedaqeel185 <syedaqeel185@gmail.com>
2022-12-11 16:38:31 +0500

    Project completed successfully

---
+++ index.js
@@ -0,0 +1,89 @@
+// // let a = ['a', 'b', 'c'];
+
+// // let b = ['d', 'e', 'f'];
+// // let c =  a.concat(b);
+// // console.log(c);
+// // // let d = prompt('Enter First name');
+// // // let e = prompt('Enter Second name');
+// // // let result = d.
+// // // concat(e).toString();
+// // // console.log(result);
+// // let str1 ="AQeel Ahmad";
+// // let part = str1.slice(2, 5);
+// // console.log(part);
+// // var arr = [];
+// // for (var i = 0; i <= 3; i++) {
+// //   arr.push(prompt("Names " + (i + 1)));
+// // }
+
+// // console.log("Names \n" + arr.join("\n "));
+
+// // function add(x, y) {
+// //   return x+y;  
+// // } 
+// // x = add(5, 5);
+// // console.log(x);
+
+
+
+// var myConcat = function (a){
+    
+//     for (i=1; i<arguments.length; i++)
+//     res += arguments[1];
+// }
+// let b=10;
+// var res= myConcat(b);
+
+// console.log(res);
+
+// function print(a, b, f, d, o){
+//     for (let i = 0; i < arguments.length; i++) {
+//         const element = arguments[i];
+//         console.log(element);
+        
+//     }
+// }
+
+
+
+
+
+
+// // print("Aqeel");
+// // print(1.2,5);
+
+// // function passByValue(a, b, c) {
+// //     console.log(a);
+// //     a=10;
+// //     console.log(a);
+
+// // }
+// // var num1={x:4};
+// // console.log(num1.x);
+// // passByValue(num1);
+// // console.log(num1.x);
+
+// const squar = function(number){
+//     return number*number;
+// }
+// var x = squar(7)
+// console.log(x);
+
+
+
+
+// hello = (a) => {
+//     var a = [];
+//     for (let i = 0; i < 2; i++) {
+//         sum= a.push(prompt("Enter values")) ;
+//         console.log(sum+= a[i]);
+
+        
+//     }
+    
+// }
+// add=(a,n)=> a+n;
+// console.log(add(1,2));
+
+var arr = ['a','5','f','r'];
+console.log(arr.copyWithin(1,3));
